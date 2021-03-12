ed12db0713ede6136934913b026d155be49cb621
Edmond <eoa@live.fr>
2021-03-12 17:35:01 +0100

    Update discover page : Add search bar

--- androidApp/src/main/java/com/caldeirasoft/outcast/ui/screen/store/directory/StoreDirectoryScreen.kt
+++ androidApp/src/main/java/com/caldeirasoft/outcast/ui/screen/store/directory/StoreDirectoryScreen.kt
@@ -4,17 +4,23 @@ package com.caldeirasoft.outcast.ui.screen.store.directory
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -27,11 +33,13 @@ import com.caldeirasoft.outcast.domain.models.store.StoreCollectionItems
 import com.caldeirasoft.outcast.domain.models.store.StoreCollectionRooms
 import com.caldeirasoft.outcast.ui.components.*
 import com.caldeirasoft.outcast.ui.navigation.Screen
+import com.caldeirasoft.outcast.ui.theme.typography
 import com.caldeirasoft.outcast.ui.util.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.flowOf
 import org.koin.core.component.KoinApiExtension
+import kotlin.math.log10
 
 enum class StoreGenreItem(val genreId: Int, @StringRes val titleId: Int, @DrawableRes val drawableId: Int) {
     Arts(1301, R.string.store_genre_1301, R.drawable.ic_color_palette),
@@ -153,14 +161,111 @@ private fun StoreDirectoryContent(
                 }
         }
 
-        ReachableAppBar(
+        ReachableAppBarWithSearchBar(
             title = {
                 Text(text = stringResource(id = R.string.store_tab_discover))
             },
             actions = {
-
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Default.Search, contentDescription = null)
+                }
             },
             state = listState,
             headerHeight = headerHeight)
+    }
+}
+
+@ExperimentalAnimationApi
+@Composable
+private fun ReachableAppBarWithSearchBar(
+    title: @Composable () -> Unit,
+    navigationIcon: @Composable (() -> Unit)? = null,
+    actions: @Composable RowScope.() -> Unit = {},
+    state: LazyListState,
+    headerHeight: Int)
+{
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(headerHeight.toDp()))
+    {
+        val scrollRatioLargeHeader =
+            if (headerHeight != 0)
+                ((headerHeight - (state.firstVisibleItemIndex * headerHeight.toFloat() + state.firstVisibleItemScrollOffset)) / headerHeight)
+                    .coerceAtLeast(0f)
+            else 1f
+        val minimumHeight = 56.dp
+        val computedHeight = (scrollRatioLargeHeader * headerHeight).toDp().coerceAtLeast(minimumHeight)
+        val alphaLargeHeader = (3 * log10(scrollRatioLargeHeader.toDouble()) + 1).toFloat().coerceIn(0f, 1f)
+        val alphaCollapsedHeader = (3 * log10((1-scrollRatioLargeHeader).toDouble()) + 1).toFloat().coerceIn(0f, 1f)
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(max = computedHeight)
+            .height(computedHeight)
+            .background(MaterialTheme.colors.background)) {
+
+            // large title
+            Box(modifier = Modifier
+                .padding(bottom = (56.dp.toIntPx() * scrollRatioLargeHeader).toDp())
+                .align(Alignment.Center)
+                .alpha(alphaLargeHeader)) {
+                ProvideTextStyle(typography.h4, title)
+            }
+
+            // top app bar
+            TopAppBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.TopStart)
+                    .alpha(alphaCollapsedHeader),
+                title = {
+                    CompositionLocalProvider(LocalContentAlpha provides alphaCollapsedHeader) {
+                        title()
+                    }
+                },
+                navigationIcon = navigationIcon,
+                actions = actions,
+                backgroundColor = Color.Transparent,
+                elevation = if (state.firstVisibleItemIndex > 0) 1.dp else 0.dp
+            )
+
+            // search bar
+            SearchBar(modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomStart)
+                .alpha(alphaLargeHeader))
+        }
+
+    }
+}
+
+@Composable
+private fun SearchBar(modifier: Modifier = Modifier)
+{
+    // search button
+    OutlinedButton(
+        onClick = {},
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp),
+        colors = ButtonDefaults.textButtonColors(
+            backgroundColor = Color.Transparent,
+            contentColor = MaterialTheme.colors.onSurface
+                .copy(alpha = ContentAlpha.medium),
+            disabledContentColor = MaterialTheme.colors.onSurface
+                .copy(alpha = ContentAlpha.disabled)
+        ),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Icon(imageVector = Icons.Filled.Search,
+                contentDescription = null,
+            )
+            Text("Search", modifier = Modifier.padding(horizontal = 4.dp))
+        }
     }
 }
