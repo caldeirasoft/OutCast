3a471385c1c8c62d91ed1c998f4e829eb348c06c
Edmond <eoa@live.fr>
2021-04-22 11:44:52 +0200

    TopSectionScreen : shimmer loading screens

--- androidApp/src/main/java/com/caldeirasoft/outcast/ui/screen/store/topchartsection/TopChartSectionScreen.kt
+++ androidApp/src/main/java/com/caldeirasoft/outcast/ui/screen/store/topchartsection/TopChartSectionScreen.kt
@@ -1,16 +1,19 @@
 package com.caldeirasoft.outcast.ui.screen.store.topchartsection
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Divider
+import androidx.compose.material.ListItem
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -21,17 +24,13 @@ import com.caldeirasoft.outcast.domain.enums.StoreItemType
 import com.caldeirasoft.outcast.domain.models.episode
 import com.caldeirasoft.outcast.domain.models.store.StoreEpisode
 import com.caldeirasoft.outcast.domain.models.store.StorePodcast
-import com.caldeirasoft.outcast.ui.components.ChipButton
-import com.caldeirasoft.outcast.ui.components.LazyListLayout
-import com.caldeirasoft.outcast.ui.components.PodcastListItem
-import com.caldeirasoft.outcast.ui.components.StoreEpisodeItem
+import com.caldeirasoft.outcast.ui.components.*
 import com.caldeirasoft.outcast.ui.components.bottomsheet.LocalBottomSheetContent
 import com.caldeirasoft.outcast.ui.components.bottomsheet.LocalBottomSheetState
 import com.caldeirasoft.outcast.ui.navigation.Screen
 import com.caldeirasoft.outcast.ui.screen.episode.EpisodeArg.Companion.toEpisodeArg
 import com.caldeirasoft.outcast.ui.screen.store.categories.CategoriesListBottomSheet
-import com.caldeirasoft.outcast.ui.util.ifLoadingMore
-import com.caldeirasoft.outcast.ui.util.mavericksViewModel
+import com.caldeirasoft.outcast.ui.util.*
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.launch
 
@@ -65,43 +64,56 @@ private fun TopChartSectionScreen(
     val state by viewModel.collectAsState()
     val lazyPagingItems = viewModel.topCharts.collectAsLazyPagingItems()
 
-    LazyListLayout(lazyListItems = lazyPagingItems) {
-        val listState = rememberLazyListState(0)
+    val listState = rememberLazyListState(0)
 
-        LazyColumn(
-            state = listState,
-            modifier = Modifier
-                .fillMaxSize()) {
+    LazyColumn(
+        state = listState,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
 
-            itemsIndexed(lazyPagingItems = lazyPagingItems) { index, item ->
-                when (item) {
-                    is StorePodcast -> {
-                        PodcastListItem(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable(onClick = {
-                                    navigateTo(Screen.PodcastScreen(item))
-                                }),
-                            storePodcast = item,
-                            index = index + 1,
-                            isFollowing = state.followingStatus.contains(item.id),
-                            isFollowingLoading = state.followLoadingStatus.contains(item.id),
-                            onSubscribeClick = viewModel::followPodcast
-                        )
-                        Divider()
-                    }
-                    is StoreEpisode -> {
-                        StoreEpisodeItem(
-                            episode = item.episode,
-                            onEpisodeClick = { navigateTo(Screen.EpisodeScreen(item.toEpisodeArg())) },
-                            onPodcastClick = { navigateTo(Screen.PodcastScreen(item.podcast)) },
-                            index = index + 1
-                        )
-                        Divider()
+        lazyPagingItems
+            .ifLoading {
+                item {
+                    TopChartSectionLoadingScreen()
+                }
+            }
+            .ifError {
+                item {
+                    ErrorScreen(t = it)
+                }
+            }
+            .ifNotLoading {
+                itemsIndexed(lazyPagingItems = lazyPagingItems) { index, item ->
+                    when (item) {
+                        is StorePodcast -> {
+                            PodcastListItem(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable(onClick = {
+                                        navigateTo(Screen.PodcastScreen(item))
+                                    }),
+                                storePodcast = item,
+                                index = index + 1,
+                                isFollowing = state.followingStatus.contains(item.id),
+                                isFollowingLoading = state.followLoadingStatus.contains(item.id),
+                                onSubscribeClick = viewModel::followPodcast
+                            )
+                            Divider()
+                        }
+                        is StoreEpisode -> {
+                            StoreEpisodeItem(
+                                episode = item.episode,
+                                onEpisodeClick = { navigateTo(Screen.EpisodeScreen(item.toEpisodeArg())) },
+                                onPodcastClick = { navigateTo(Screen.PodcastScreen(item.podcast)) },
+                                index = index + 1
+                            )
+                            Divider()
+                        }
                     }
                 }
             }
-            lazyPagingItems.ifLoadingMore {
+            .ifLoadingMore {
                 item {
                     Text(
                         modifier = Modifier.padding(
@@ -112,9 +124,48 @@ private fun TopChartSectionScreen(
                     )
                 }
             }
-            item {
-                // bottom app bar spacer
-                Spacer(modifier = Modifier.height(56.dp))
+    }
+}
+
+@Composable
+fun TopChartSectionLoadingScreen() {
+    LoadingListShimmer { list, floatAnim ->
+        val brush = Brush.verticalGradient(list, 0f, floatAnim)
+        Column(modifier = Modifier
+            .padding(vertical = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+
+        ) {
+            // episodes
+            Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                repeat(12) {
+                    ListItem(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = {
+                            Spacer(modifier = Modifier
+                                .fillMaxWidth()
+                                .height(14.dp)
+                                .background(brush = brush))
+                        },
+                        secondaryText = {
+                            Column(
+                                verticalArrangement = Arrangement.spacedBy(3.dp)) {
+                                repeat(3) {
+                                    Spacer(modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(10.dp)
+                                        .background(brush = brush))
+                                }
+                            }
+                        },
+                        icon = {
+                            Spacer(modifier = Modifier
+                                .size(40.dp)
+                                .background(brush = brush))
+                        }
+                    )
+                }
             }
         }
     }
